5dd80906c4112c437276fb6bf32df1865c2c5437
petrovicjana <jance.petrovic2003@gmail.com>
2024-08-09 14:48:43 +0200

    Updated settings.py
    Updated settings.py

--- ModelPredictionSystem/ModelPredictionSystem/settings.py
+++ ModelPredictionSystem/ModelPredictionSystem/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-from ModelPredictionSystem import ModelPredictionSystem
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
